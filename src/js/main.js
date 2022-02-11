12263a914695dc96da4515b00a3f5add5ff7758a
sifat88 <rakibulislam8sifat@gmail.com>
2022-02-11 22:59:15 +0600

    updated homepage

--- src/js/main.js
+++ src/js/main.js
@@ -2,30 +2,32 @@ import { initIcons } from "@47nordmedia/k2-systems-ui-library/dist/icons";
 
 const menuButton = document.querySelector(".menu-toggle");
 const navBlock = document.querySelector("nav");
+const body = document.querySelector("body");
 
-// menuButton.addEventListener("click", function () {
-//   this.classList.toggle("show");
-//   if (this.classList.contains("show")) {
-//     this.innerHTML = "Close";
-//   } else {
-//     this.innerHTML = "Menü";
-//   }
-//   navBlock.classList.toggle("inline-flex");
-//   navBlock.classList.toggle("hidden");
-// });
+menuButton.addEventListener("click", function () {
+  this.classList.toggle("show");
+  if (this.classList.contains("show")) {
+    this.innerHTML = "Close";
+  } else {
+    this.innerHTML = "Menü";
+  }
+  body.classList.toggle("overflow-hidden");
+  navBlock.classList.toggle("inline-flex");
+  navBlock.classList.toggle("hidden");
+});
 
-// // Footer links accordion
+// links accordion
 
-// const footerLinks = document.querySelectorAll("p.footerLinks");
+const footerLinks = document.querySelectorAll("p.footerLinks , p.menuLink");
 
-// footerLinks.forEach((p) => {
-//   const minus = p.querySelector("svg > path:last-of-type");
-//   const LinkList = p.nextElementSibling;
-//   p.addEventListener("click", () => {
-//     minus.classList.toggle("hidden");
-//     LinkList.classList.toggle("hidden");
-//   });
-// });
+footerLinks.forEach((p) => {
+  const minus = p.querySelector("svg > path:last-of-type");
+  const LinkList = p.nextElementSibling;
+  p.addEventListener("click", () => {
+    minus.classList.toggle("hidden");
+    LinkList.classList.toggle("hidden");
+  });
+});
 
 initIcons();
 
