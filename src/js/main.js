be195c1d0cbcc0b875f5ab011eeb86806936697e
sifat88 <rakibulislam8sifat@gmail.com>
2022-08-27 11:11:57 +0600

    Addded wrapper/container

--- src/js/main.js
+++ src/js/main.js
@@ -34,82 +34,82 @@ if (menuButton) {
 }
 
 // Search & language selection modal ----------
-// const eachBtn = document.querySelectorAll(".search-btn, .lang-btn");
-// const navModalContainer = document.querySelector(".navmodal-container");
-// const navModalWrapper = document.querySelector(".navmodal-wrapper");
-// const navSearchWrapper = document.querySelector(".navsearch-wrapper");
-// const navmodalClose = navModalContainer.querySelector("button.navclose-btn");
-// const navSearchClose = navModalContainer.querySelector(
-//   "button.searchclose-btn"
-// );
-// if (navModalContainer) {
-//   function modalOpenFunc() {
-//     navModalContainer.classList.toggle("opacity-0");
-//     navModalContainer.classList.toggle("invisible");
-//     body.classList.toggle("overflow-hidden");
-//   }
-//   let searchClicked = 0;
-//   eachBtn.forEach((item) => {
-//     item.addEventListener("click", (e) => {
-//       e.preventDefault();
-//       if (item.classList.contains("search-btn")) {
-//         navModalContainer.classList.add("justify-center");
-//         navModalContainer.classList.add("items-center");
-//         navModalWrapper.classList.add("hidden");
-//         navSearchWrapper.classList.remove("hidden");
-//         window.innerWidth < 1024 && navSearchWrapper.classList.add("!top-0");
-//         modalOpenFunc();
-//         setTimeout(() => {
-//           navSearchWrapper.querySelector("input").focus();
-//         }, 500);
-//       } else if (item.classList.contains("lang-btn")) {
-//         navModalContainer.classList.remove("justify-center");
-//         navModalContainer.classList.remove("items-center");
-//         navSearchWrapper.classList.add("hidden");
-//         window.innerWidth < 1024 && navSearchWrapper.classList.remove("!top-0");
-//         modalOpenFunc();
-//         navModalWrapper.classList.toggle("left-full");
-//         navModalWrapper.classList.toggle("left-0");
-//         searchClicked = 1;
-//       }
-//     });
-//   });
-//   navModalContainer.addEventListener("click", (e) => {
-//     if (
-//       e.target === navModalContainer ||
-//       e.target === navmodalClose ||
-//       e.target.closest("button.searchclose-btn") === navSearchClose
-//     ) {
-//       modalOpenFunc();
-//       setTimeout(() => {
-//         navModalWrapper.classList.remove("hidden");
-//       }, 500);
-//       if (searchClicked === 1) {
-//         navModalWrapper.classList.toggle("left-full");
-//         navModalWrapper.classList.toggle("left-0");
-//         searchClicked = 0;
-//       } else if (searchClicked === 0 && window.innerWidth < 1024) {
-//         window.innerWidth < 1024 && navSearchWrapper.classList.remove("!top-0");
-//       }
-//     }
-//   });
+const eachBtn = document.querySelectorAll(".search-btn, .lang-btn");
+const navModalContainer = document.querySelector(".navmodal-container");
+const navModalWrapper = document.querySelector(".navmodal-wrapper");
+const navSearchWrapper = document.querySelector(".navsearch-wrapper");
+const navmodalClose = navModalContainer.querySelector("button.navclose-btn");
+const navSearchClose = navModalContainer.querySelector(
+  "button.searchclose-btn"
+);
+if (navModalContainer) {
+  function modalOpenFunc() {
+    navModalContainer.classList.toggle("opacity-0");
+    navModalContainer.classList.toggle("invisible");
+    body.classList.toggle("overflow-hidden");
+  }
+  let searchClicked = 0;
+  eachBtn.forEach((item) => {
+    item.addEventListener("click", (e) => {
+      e.preventDefault();
+      if (item.classList.contains("search-btn")) {
+        navModalContainer.classList.add("justify-center");
+        navModalContainer.classList.add("items-center");
+        navModalWrapper.classList.add("hidden");
+        navSearchWrapper.classList.remove("hidden");
+        window.innerWidth < 1024 && navSearchWrapper.classList.add("!top-0");
+        modalOpenFunc();
+        setTimeout(() => {
+          navSearchWrapper.querySelector("input").focus();
+        }, 500);
+      } else if (item.classList.contains("lang-btn")) {
+        navModalContainer.classList.remove("justify-center");
+        navModalContainer.classList.remove("items-center");
+        navSearchWrapper.classList.add("hidden");
+        window.innerWidth < 1024 && navSearchWrapper.classList.remove("!top-0");
+        modalOpenFunc();
+        navModalWrapper.classList.toggle("left-full");
+        navModalWrapper.classList.toggle("left-0");
+        searchClicked = 1;
+      }
+    });
+  });
+  navModalContainer.addEventListener("click", (e) => {
+    if (
+      e.target === navModalContainer ||
+      e.target === navmodalClose ||
+      e.target.closest("button.searchclose-btn") === navSearchClose
+    ) {
+      modalOpenFunc();
+      setTimeout(() => {
+        navModalWrapper.classList.remove("hidden");
+      }, 500);
+      if (searchClicked === 1) {
+        navModalWrapper.classList.toggle("left-full");
+        navModalWrapper.classList.toggle("left-0");
+        searchClicked = 0;
+      } else if (searchClicked === 0 && window.innerWidth < 1024) {
+        window.innerWidth < 1024 && navSearchWrapper.classList.remove("!top-0");
+      }
+    }
+  });
 
-//   // Lang select ------
-//   const langList = navModalWrapper.querySelectorAll("ul.lang-list > li");
-//   langList.forEach((li) => {
-//     li.addEventListener("click", () => {
-//       langList.forEach((li) => {
-//         li.classList.remove("lang-selected");
-//         li.querySelector(".lang-tick").classList.add("invisible");
-//       });
-//       const langTick = li.querySelector(".lang-tick");
-//       li.classList.add("lang-selected");
-//       if (langTick.closest(".lang-selected")) {
-//         langTick.classList.remove("invisible");
-//       }
-//     });
-//   });
-// }
+  // Lang select ------
+  const langList = navModalWrapper.querySelectorAll("ul.lang-list > li");
+  langList.forEach((li) => {
+    li.addEventListener("click", () => {
+      langList.forEach((li) => {
+        li.classList.remove("lang-selected");
+        li.querySelector(".lang-tick").classList.add("invisible");
+      });
+      const langTick = li.querySelector(".lang-tick");
+      li.classList.add("lang-selected");
+      if (langTick.closest(".lang-selected")) {
+        langTick.classList.remove("invisible");
+      }
+    });
+  });
+}
 // Horizontal slider -----------
 const sliders = document.querySelectorAll(".drag-scroll");
 
